fccb89796172130ad29dc56a050a8e2d1e64d8f4
Nick <nickmagi24@gmail.com>
2021-08-31 12:43:03 -0400

    Building

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.getElementById('#container');
+
+function makeRows(row, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (i = 0; i < (rows * cols); i++) {
+        let cell = document.createElement("div");
+        cell.innerText = (i + 1);
+        container.appendChild(cell),className = "grid-item";
+    };
+};
+
