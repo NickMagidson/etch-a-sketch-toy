4199d217c19a407b892b965622679f91ea49534a
Nick <nickmagi24@gmail.com>
2021-08-31 13:41:04 -0400

    div blocks function

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-const container = document.getElementById('#container');
+const container = document.getElementById('container');
+let rows = document.getElementsByClassName("gridRow");
+let cells = document.getElementsByClassName("cell");
 
 function makeRows(row, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
-    for (i = 0; i < (rows * cols); i++) {
+    for (let i = 0; i < (rows * cols); i++) {
         let cell = document.createElement("div");
         cell.innerText = (i + 1);
         container.appendChild(cell),className = "grid-item";
     };
 };
 
+makeRows(16, 16);
