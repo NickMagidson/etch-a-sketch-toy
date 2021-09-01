8d9fa64219739c704a7a69e8b7ef24a45f986e6f
Nick <nickmagi24@gmail.com>
2021-09-01 13:54:13 -0400

    Comments

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
+// Declaring important variables
 const container = document.getElementById('container');
 let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName("cell");
 
-
+// Makes 16x16 grid
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
 
     for (c = 0; c < (rows * cols); c++) {
-
       let cell = document.createElement("div");
-      
-
       cell.addEventListener('mouseover', function() {
-
+        // Generates random color
         let rcolor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-  
         cell.style.backgroundColor = rcolor;
-  
       });
-
       container.appendChild(cell).className = "grid-item";
   };
 };
@@ -27,18 +22,16 @@ function makeRows(rows, cols) {
 makeRows(16, 16);
 
 
+// Removes child nodes from parent container
 function removeAllChildNodes(parent){
     while(parent.firstChild){
         parent.removeChild(parent.firstChild);
     }
 }
 
-
+// Reset button
 const reset = document.querySelector('#reset');
 reset.addEventListener('click', function() {
-    
     removeAllChildNodes(container);
-    
     makeRows(16, 16);
-    
 });
